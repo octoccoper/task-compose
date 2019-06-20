14f071c0e738050a8f33e9fced72adbe564122ee
octoccoper <octocopper@gmail.com>
2019-06-20 15:40:15 +0300

    added solution.js with formatted data

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
 function compose(fns) {
-  return function (data) {
-    return fns.reduceRight(function (value, func) {
+  return function(data) {
+    return fns.reduceRight(function(value, func) {
       return func(value);
     }, data);
   };
-};
+}
