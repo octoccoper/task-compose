dcc9c168b158ba557807aebf9b86ecec068612a4
octoccoper <octocopper@gmail.com>
2019-06-26 12:58:04 +0300

    update solution.js

--- solution.js
+++ solution.js
@@ -1,7 +1,10 @@
 function compose(fns) {
-  return function(...args) {
-    return fns.reduceRight(function(value, func) {
-      return func(value);
-    }, fns.pop()(...args));
+  return function composition(...args) {
+    return fns.reduceRight(function(values, func) {
+      if (values.length === 2) {
+        return func(values[0], values[1]);
+      }
+      return func(values);
+    }, args);
   };
 }
