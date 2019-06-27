d7f6036c7b967ddec09a5c813d34b806a5a49c2f
octoccoper <octocopper@gmail.com>
2019-06-27 14:33:25 +0300

    update js

--- solution.js
+++ solution.js
@@ -1,19 +1,7 @@
 function compose(fns) {
   return function(...args) {
-    console.log(
-      "anon funct: type of args is:",
-      typeof args,
-      " args is: ",
-      args
-    );
     return fns.reduceRight(function(args, func) {
-      console.log(
-        "inside reduceRight: type of args is:",
-        typeof args,
-        " args is: ",
-        args
-      );
-      return Array.isArray(args) ? func(...args) : func(args);
-    }, args);
+      return [func(...args)];
+    }, args)[0];
   };
 }
